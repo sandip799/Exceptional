1bbcf1fe30de7d3fd8754bd09949dfd14ffd8c5f
Sandip Kumar Swain <79470488+sandip799@users.noreply.github.com>
2021-04-20 11:55:46 +0530

    Add files via upload
    Write a C program that reads an expression and evaluates it.
    Input:
    4
    10-2*3=
    8*(8+2-5)=

---
+++ math_Expression_string.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+char str[100];
+int ind=0;
+//expression check function
+int check()
+{
+    int n=0;
+    while(1)
+    {
+        if(str[ind]>='0' && str[ind]<='9')
+        {
+            n=n*10 + (str[ind]-'0');
+            ind++;
+        }
+        else{
+            return n;
+        }
+    }
+}
+//multiplication & division function
+int muldiv()
+{
+    int first,second;
+    first=check();
+    while(1)
+    {
+        if(str[ind]=='*')
+        {
+            ind++;
+            second=check();
+            first=first*second;
+        }
+        else if(str[ind]=='/')
+        {
+            ind++;
+            second=check();
+            first=first/second;
+        }
+        else
+        {
+            return first;
+        }
+    }
+}
+//function for addition and substraction
+int addsub()
+{
+    int first,second;
+    first=muldiv();
+    while(1)
+    {
+        if(str[ind]=='+')
+        {
+            ind++;
+            second=muldiv();
+            first=first+second;
+        }
+        else if(str[ind]=='-')
+        {
+            ind++;
+            second=muldiv();
+            first=first-second;
+        }
+        else
+        {
+            return first;
+        }
+    }
+}
+int main()
+{
+    printf("Enter The Mathematical Expression\n");
+    scanf("%s",str);
+    int result=addsub();
+    printf("= %d",result);
+return 0;
+}
